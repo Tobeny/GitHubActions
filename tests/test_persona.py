287bdd622597c53162474834d023766179144555
Benito Zarate <bzaratepalomec@gmail.com>
2022-09-22 07:37:38 -0500

    ifs(Rojo)

--- tests/test_persona.py
+++ tests/test_persona.py
@@ -72,8 +72,11 @@ class PersonaTestCase(unittest.TestCase):
     def test8(self):
         self.assertEqual(True, True)
     
-    def test8(self):
-        self.assertEqual(True, True)
-    
     def test9(self):
         self.assertEqual(True, True)
+    
+    def test10(self):
+        self.assertEqual(True, True)
+    
+    def test11(self):
+        self.assertEqual(True, False)
